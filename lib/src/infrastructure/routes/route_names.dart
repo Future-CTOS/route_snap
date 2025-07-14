5e7b0f8d94c37eba48806f003b6cc2edf5b5a3c7
shayanzare-dev <shayanzare2222@gmail.com>
2025-07-15 03:07:11 +0330

    feat: Add route_names & route_paths

---
+++ lib/src/infrastructure/routes/route_names.dart
@@ -0,0 +1,9 @@
+import 'route_paths.dart';
+
+class RouteNames {
+  const RouteNames._();
+
+  // const makes cache system!
+  static const String splash = RoutePaths.splash;
+  static const String mapSelector = RoutePaths.mapSelector;
+}
---
+++ lib/src/infrastructure/routes/route_paths.dart
@@ -0,0 +1,6 @@
+class RoutePaths {
+  const RoutePaths._();
+
+  static const String splash = '/splash';
+  static const String mapSelector = '/map-selector';
+}
