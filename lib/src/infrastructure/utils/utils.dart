bf1a94fc4711c7e2d5faa5c6f29bcd61b8f435f3
shayanzare-dev <shayanzare2222@gmail.com>
2025-07-15 03:04:18 +0330

    feat: Add utils.dart

---
+++ lib/src/infrastructure/utils/utils.dart
@@ -0,0 +1,12 @@
+import 'package:custom_map_plugin/custom_map_export.dart';
+
+class Utils {
+  Utils._();
+
+  static const double tinySpace = 2;
+  static const double smallSpace = 4;
+  static const double semiMediumSpace = 6;
+  static const double largeSpace = 16;
+
+  static LatLng get defaultLocation => LatLng(29.5926, 52.5836);
+}
