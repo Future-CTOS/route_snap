675b37cb882eb76b73b46c7e8e85f6c103f302ef
shayanzare-dev <shayanzare2222@gmail.com>
2025-07-15 02:59:38 +0330

    feat: add App file

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'infrastructure/routes/app_route_pages.dart';
+import 'infrastructure/routes/route_paths.dart';
+
+class App extends StatelessWidget {
+  final String appName;
+
+  const App({required this.appName, super.key});
+
+  @override
+  Widget build(BuildContext context) => GetMaterialApp(
+    title: appName,
+    debugShowCheckedModeBanner: false,
+    // theme: DarkLightTheme.theme,
+    initialRoute: RoutePaths.mapSelector,
+    getPages: AppRoutePages.routes,
+  );
+}
